303e96eafd6f42ee948523239e6629743fbf857c
ShanXuerui <764795838@qq.com>
2020-03-19 23:34:53 +0800

    推送Test

---
+++ Day11/src/annotation/TuiSongTest.java
@@ -0,0 +1,11 @@
+package annotation;
+
+/**
+ * Author:单雪瑞
+ * 2020/3/19 23:34
+ */
+public class TuiSongTest {
+    public static void main(String[] args) {
+        System.out.println("推送Test");
+    }
+}
