247bbde4046cea6fe733391a3d0eb3cba0a767b9
shanxuerui <53455011+shanxuerui@users.noreply.github.com>
2020-03-19 23:32:13 +0800

    Update DevTest.java

--- Day11/src/annotation/DevTest.java
+++ Day11/src/annotation/DevTest.java
@@ -6,6 +6,6 @@ package annotation;
  */
 public class DevTest {
     public static void main(String[] args) {
-        System.out.println("devTest");
+        System.out.println("devTestSleep");
     }
 }
