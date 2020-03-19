d4e4e98db2c422d03767ce3ba161be7cd5a45057
ShanXuerui <764795838@qq.com>
2020-03-19 23:27:09 +0800

    devTest

---
+++ Day11/src/annotation/DevTest.java
@@ -0,0 +1,11 @@
+package annotation;
+
+/**
+ * Author:单雪瑞
+ * 2020/3/19 23:26
+ */
+public class DevTest {
+    public static void main(String[] args) {
+        System.out.println("devTest");
+    }
+}
